58cf5fa0c8e6ee1d661418054b29165c8312e673
gianpc <gianpc@gmail.com>
2025-06-30 12:34:04 -0500

    forma sincrona de leer archivos

--- 3.fs-readfile.js
+++ 3.fs-readfile.js
@@ -1,4 +1,14 @@
 const fs = require('node:fs')
+
+// Este script lee dos archivos de texto de forma síncrona y muestra su contenido en la consola.
+console.log('Leyendo el archivo n° 1 ...')
 const text = fs.readFileSync('./archivo.txt', 'utf-8')
 
 console.log(text)
+
+console.log('Hacer cosas mientras lee el archivo n° 1 ...') // No lo puede hacer porque es síncrono
+
+console.log('Leyendo el archivo n° 2 ...')
+const secondText = fs.readFileSync('./archivo2.txt', 'utf-8')
+
+console.log(secondText)

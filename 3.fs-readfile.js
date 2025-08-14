e77632c439438be241a00d27f293813e8ce37101
gianpc <gpaucarcortez@gmail.com>
2025-08-14 18:25:06 -0500

    Resultado de la forma sincrona

--- 3.fs-readfile.js
+++ 3.fs-readfile.js
@@ -1,14 +1,26 @@
 const fs = require('node:fs')
 
 // Este script lee dos archivos de texto de forma síncrona y muestra su contenido en la consola.
-console.log('Leyendo el archivo n° 1 ...')
-const text = fs.readFileSync('./archivo.txt', 'utf-8')
+console.log('Leyendo el archivo n° 1 ...') // --> 1
+const text = fs.readFileSync('./archivo.txt', 'utf-8') // --> 2
 
 console.log(text)
 
-console.log('Hacer cosas mientras lee el archivo n° 1 ...') // No lo puede hacer porque es síncrono
+console.log('Hacer cosas mientras lee el archivo n° 1 ...') // No lo puede hacer porque es síncrono --> 3
 
-console.log('Leyendo el archivo n° 2 ...')
-const secondText = fs.readFileSync('./archivo2.txt', 'utf-8')
+console.log('Leyendo el archivo n° 2 ...') // --> 4
+const secondText = fs.readFileSync('./archivo2.txt', 'utf-8') // --> 5
 
 console.log(secondText)
+
+/**
+ * -------- Salida --------
+ * Leyendo el archivo n° 1 ...
+ * Hola mundo
+ * Hacer cosas mientras lee el archivo n° 1 ...
+ * Leyendo el archivo n° 2 ...
+ * gian
+ * carlos
+ * paucar
+ * cortez
+ */
